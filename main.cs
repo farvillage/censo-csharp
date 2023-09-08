b7f7403200a183eec80213127d4fddfad055e02d
farvillage <ygorsoares2003@gmail.com>
2023-09-08 12:33:01 -0300

    yay

--- main.cs
+++ main.cs
@@ -1,59 +1,66 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
- public class Program
- {
-   public static void Main(string[] args)
-   {
-    
-    for(int i = 0; i < 3; i++)
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    int addHeight = 0;
+    double averageHeight = 0;
+    int countM = 0;
+    int countF = 0;
+    int maxHeight = 0;
+    int minHeight = 300;
+
+    for (int i = 0; i < 3; i++)
     {
       Console.WriteLine("Input your sex (M or F): ");
       string inputSex = Console.ReadLine();
-        
-      Console.WriteLine("Input your height (cm): "); 
+
+      Console.WriteLine("Input your height (cm): ");
       string inputHeight = Console.ReadLine();
 
       int input = int.Parse(inputHeight);
-      double averageMen = 0.0;
-      
-      Person person = new Person(inputSex, input);
-      int maxHeight = 0;
-      int minHeight = 0;
-      
-      if(inputSex.ToLower().StartsWith("f"))
-      {
-        if(person.height > maxHeight)
-        {
-          maxHeight = person.height;
-        }
-        if(person.height < minHeight)
-        {
-          minHeight = person.height;
-        }
-        
-        if (averageMen = averageMen / person.height)
-        {
-          Console.WriteLine("The average height is" + person.height);
-        }
 
-        if(person.sex == "f")
-        {
-          person.sex 1++;
-          Console.WriteLine("The quantity of women is " + person.sex);
-        }
+      Person person = new Person(inputSex, input);
+
+      if (person.height > maxHeight)
+      {
+        maxHeight = person.height;
+      }
+      if (person.height < minHeight)
+      {
+        minHeight = person.height;
+      }
+
+      if (person.sex == "m")
+      {
+        countM =+ 1;
+        addHeight = person.height + addHeight;
+      }
+
+      if (person.sex == "f")
+      {
+        countF =+ 1;
       }
     }
-   }
+    averageHeight = addHeight / countM;
+
+    Console.WriteLine("The quantity of men is " + countM);
+    Console.WriteLine($"The min and max men's height is {minHeight} {maxHeight}");
+    Console.WriteLine("The quantity of women is " + countF);
+    Console.WriteLine("The average of men's height is " + averageHeight);
+  }
 }
 
- public class Person
- {
-    public String sex;
-    public int height;
+public class Person
+{
+  public String sex;
+  public int height;
 
-    public Person(String sex, int height)
-    {
+  public Person(String sex, int height)
+  {
     this.sex = sex;
     this.height = height;
-    }
- }
+  }
+}
